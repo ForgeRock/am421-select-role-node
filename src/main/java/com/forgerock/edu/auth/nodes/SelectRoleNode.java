bb945e66c2de262a9a60a2ec66bbd41e10e24037
Peter Varga (VM) <peter.varga@dpc.hu>
2018-11-22 04:18:50 -0500

    Solution: v1

--- src/main/java/com/forgerock/edu/auth/nodes/SelectRoleNode.java
+++ src/main/java/com/forgerock/edu/auth/nodes/SelectRoleNode.java
@@ -15,6 +15,7 @@
  */
 package com.forgerock.edu.auth.nodes;
 
+import com.google.common.collect.ImmutableList;
 import com.google.inject.assistedinject.Assisted;
 import com.iplanet.sso.SSOException;
 import com.iplanet.sso.SSOToken;
@@ -31,6 +32,7 @@ import java.security.AccessController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import org.forgerock.openam.annotations.sm.Attribute;
@@ -38,6 +40,10 @@ import org.forgerock.openam.auth.node.api.*;
 import org.forgerock.openam.core.CoreWrapper;
 
 import javax.inject.Inject;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.ChoiceCallback;
+
+import static org.forgerock.openam.auth.node.api.Action.send;
 
 import static org.forgerock.openam.auth.node.api.SharedStateConstants.REALM;
 import static org.forgerock.openam.auth.node.api.SharedStateConstants.USERNAME;
@@ -83,26 +89,96 @@ public class SelectRoleNode extends SingleOutcomeNode {
         this.coreWrapper = coreWrapper;
     }
 
-    @Override
+ @Override
     public Action process(TreeContext context) throws NodeProcessException {
-        String username = "demo";
-        AMIdentity userIdentity = coreWrapper.getIdentity(username, context.sharedState.get(REALM).asString());
-
-        try {
-            if (userIdentity != null && userIdentity.isExists()
-                    && userIdentity.isActive()) {
-                return goToNext().replaceSharedState(
-                        context.sharedState.copy().put(
-                                USERNAME, username)).build();
+        String username = context.sharedState.get(USERNAME).asString();
+        String realm = context.sharedState.get(REALM).asString();
+        
+        AMIdentity userIdentity = coreWrapper.getIdentity(username, realm);
+        
+        // Determine the common roles to create the final selectable list of roles.
+        List<String> selectableRoles = getUserAssignedRoles(userIdentity);
+        selectableRoles.retainAll(config.allRealmRoles());
+        
+        if (context.hasCallbacks()) {
+            Action.ActionBuilder actionB = goToNext();
+            //JsonValue copyState = context.sharedState.copy();
+            Iterator<? extends Callback> iterator = context.getAllCallbacks().iterator();
+            while (iterator.hasNext()) {
+                ChoiceCallback choiceCb = (ChoiceCallback) iterator.next();
+                int countSelected = choiceCb.getSelectedIndexes().length;
+                if (countSelected > 1) {
+                    debug.error(this.getClass().getSimpleName() + " Too many choices selected, cannot determine role to use.");
+                    throw new NodeProcessException("Multiple roles choices is not supported, please choose one role");
+                }
+                else {
+                    int selectedIdx = choiceCb.getSelectedIndexes()[0];
+                    //debug.message("Selected Index: " + selectedIdx);
+                    String selectedRole = choiceCb.getChoices()[selectedIdx];
+                    debug.error("Choice from callack set as session selectedRole: " + selectedRole);
+                    // Add selectedRole to session properties
+                    // and to the shared state
+                    actionB.putSessionProperty("selectedRole", selectedRole);     
+                    //copyState.put("selectedRole", selectedRole);
+                }
             }
-        } catch (IdRepoException e) {
-            debug.error("[" + DEBUG_FILE + "]: "
-                    + "Error locating user '{}' ", e);
-        } catch (SSOException e) {
-            debug.error("[" + DEBUG_FILE + "]: "
-                    + "Error locating user '{}' ", e);
+            return actionB.build(); //  .replaceSharedState(context.sharedState.copy().put(USERNAME, username)).build();
         }
-        return goToNext().build();
+        else {
+            int numberOfRoles = selectableRoles.size();
+            // Set the default role as the selectedRole if the number of selectable roles is zero.
+            String selectedRole = config.defaultRole();
+            
+            //debug.message("Number of selectableRoles: " + numberOfRoles + ", selectedRole " + (numberOfRoles == 0 ? "(default): " : "(initial): ") + selectedRole);            
+            if (numberOfRoles < 2) {
+                // If numberOfRoles is one then set selectedRole to the first and only choice
+                if (numberOfRoles == 1) selectedRole = selectableRoles.get(0);
+                Action.ActionBuilder actionB = goToNext();
+                //JsonValue copyState = context.sharedState.copy();
+                actionB.putSessionProperty("selectedRole", selectedRole);  
+                // Optionally, add the selectedRole into sharedState to use in a Scripted Deciscion node
+                // copyState.put("selectedRole", selectedRole); 
+                debug.error("Set session selectedRole: " + (numberOfRoles == 0 ? "(default) " : "(single) ")  + selectedRole);
+                return actionB.build(); //replaceSharedState(copyState).build();
+            }
+            else { // more than one choice, build the callback
+                //List<Callback> callbacks = new ArrayList<Callback>(1);
+                String[] choices = new String[numberOfRoles];
+                selectableRoles.toArray(choices);
+                ChoiceCallback choiceCallback = new ChoiceCallback("Select Role", choices, 0, false);
+                choiceCallback.setSelectedIndex(0);
+                //debug.message("Get ChoiceCallback name: " + choiceCallback.getPrompt() + " default choice: " + choiceCallback.getDefaultChoice());
+                //callbacks.add(choiceCallback);
+                //return send(ImmutableList.copyOf(callbacks)).build();
+                return send(ImmutableList.of(choiceCallback)).build();
+            }             
+        }
+    }
+    
+    private List<String> getUserAssignedRoles(AMIdentity userIdentity) throws NodeProcessException {
+        List<String> allUserRoles = new ArrayList<>(); 
+        try {
+            if (userIdentity != null && userIdentity.isExists() && userIdentity.isActive()) {
+                try {
+                    Set<AMIdentity> memberships = userIdentity.getMemberships(IdType.GROUP);
+                    for (AMIdentity role : memberships) {
+                        allUserRoles.add(role.getName());   
+                    }
+                }
+                catch (IdRepoException ex) {
+                    debug.error("Exception during querying groups...", ex);
+                    // Due to this exception, answer will be empty;
+                    throw new NodeProcessException(ex.getMessage());
+                }
+            }
+            else {
+                throw new NodeProcessException("User " + userIdentity.getName() + " either does not exist or is not active.");
+            }
+        }
+        catch (IdRepoException | SSOException ex) {
+            throw new NodeProcessException(ex.getMessage());
+        }
+        return allUserRoles;  // returns either an empty list, or a list with one or more roles.
     }
 
     private static List<String> getAllRealmRoles() {
