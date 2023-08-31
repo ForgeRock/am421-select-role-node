fec72907c15e620d2120f59ca926386e82aed6f2
Geza Simon <geza.simon@dpc.hu>
2023-08-31 23:42:20 +0200

    Using IdUtiles for repo, test code calling new TreeContext constructor, pom.xml updated and sanitized for 7.3.0

---
+++ src/main/java/com/forgerock/edu/auth/nodes/AmIdentityHelper.java
@@ -0,0 +1,81 @@
+package com.forgerock.edu.auth.nodes;
+
+import com.iplanet.sso.SSOException;
+import com.iplanet.sso.SSOToken;
+import com.sun.identity.idm.*;
+import com.sun.identity.security.AdminTokenAction;
+
+import java.security.AccessController;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class AmIdentityHelper {
+
+    /**
+     * Retrieves all the assigned groups of the given userIdentity object.
+     *
+     * @param userIdentity AMIdentity instance representing the user, whose memberships should be returned.
+     * @return Set of AMIdentity instances representing the groups that the given user is member of.
+     * @throws IllegalArgumentException if the passed AMIdentity instance is {@code null}, it is not a USER identity, it does not exist or it is inactive.
+     * @throws IdRepoException If an error occurs in the IdRepo during querying the memberships of the given identity
+     * @throws SSOException If the user's SSO token is invalid.
+     */
+    // DONE Ch2L2Ex2 Task2: Observe the usage the AMIdentity class
+    public Set<AMIdentity> findAllAssignedGroupsOfUser(AMIdentity userIdentity) throws IllegalArgumentException, IdRepoException, SSOException {
+        if (userIdentity == null
+                || userIdentity.getType() != IdType.USER
+                || !userIdentity.isExists()
+                || !userIdentity.isActive()) {
+            throw new IllegalArgumentException("User either does not exist or is not active.");
+        } else {
+            return userIdentity.getMemberships(IdType.GROUP);
+        }
+    }
+
+
+    public Set<String> findAllAssignedGroupNamesOfUser(AMIdentity userIdentity) throws IllegalArgumentException, IdRepoException, SSOException {
+        return findAllAssignedGroupsOfUser(userIdentity)
+                .stream()
+                .map(AMIdentity::getName)
+                .collect(Collectors.toSet());
+    }
+
+
+    /**
+     * Retrieves all groups in the given realm.
+     *
+     * @throws IdRepoException If there are repository related error conditions
+     * @throws SSOException If the admin's single sign on token is invalid.
+     */
+    public Set<AMIdentity> findAllGroupsInRealm(String realm) throws IdRepoException, SSOException {
+        // DONE Ch2L2Ex2 Task2: Observe the way to obtain an admin SSO Token for privileged operations
+        SSOToken adminToken = (SSOToken) AccessController.doPrivileged(AdminTokenAction.getInstance());
+
+        // DONE Ch2L2Ex2 Task2: Observe the creation of AMIdentityRepository instance
+        // DONE Ch2L2Ex2 Task2:   the first parameter specifies the ream
+        // DONE Ch2L2Ex2 Task2:   the second parameter determines the performer's identity
+        AMIdentityRepository identityRepository
+                = new AMIdentityRepository(realm, adminToken);
+
+        IdSearchControl searchControl = new IdSearchControl();
+        // DONE Ch2L2Ex2 Task2: Observe the way to query for all the groups within a specified realm
+        IdSearchResults searchResult = identityRepository.searchIdentities(
+                IdType.GROUP, "*", searchControl);
+        return searchResult.getSearchResults();
+    }
+
+    /**
+     * Retrieves all group names of the given realm
+     * @param realm
+     * @return
+     * @throws IdRepoException
+     * @throws SSOException
+     */
+    public Set<String> findAllGroupNamesInRealm(String realm) throws IdRepoException, SSOException {
+        return findAllGroupsInRealm(realm)
+                .stream()
+                .map(group -> group.getName())
+                .collect(Collectors.toSet());
+    }
+
+}
--- src/main/java/com/forgerock/edu/auth/nodes/SelectRoleNodePlugin.java
+++ src/main/java/com/forgerock/edu/auth/nodes/SelectRoleNodePlugin.java
@@ -62,63 +62,64 @@ import org.slf4j.LoggerFactory;
  */
 public class SelectRoleNodePlugin extends AbstractNodeAmPlugin {
 
-	static private String currentVersion = "1.0.0";
-	
-    /** 
-     * Specify the Map of list of node classes that the plugin is providing. These will then be installed and
-     *  registered at the appropriate times in plugin lifecycle.
-     *
-     * @return The list of node classes.
-     */
+	static private String currentVersion = "2.0";
+
+	/**
+	 * Specify the Map of list of node classes that the plugin is providing. These will then be installed and
+	 *  registered at the appropriate times in plugin lifecycle.
+	 *
+	 * @return The list of node classes.
+	 */
 	@Override
 	protected Map<String, Iterable<? extends Class<? extends Node>>> getNodesByVersion() {
-		return Collections.singletonMap(SelectRoleNodePlugin.currentVersion, 
+		return Collections.singletonMap(SelectRoleNodePlugin.currentVersion,
 				Collections.singletonList(SelectRoleNode.class));
 	}
 
-    /** 
-     * Handle plugin installation. This method will only be called once, on first AM startup once the plugin
-     * is included in the classpath. The {@link #onStartup()} method will be called after this one.
-     * 
-     * No need to implement this unless your AuthNode has specific requirements on install.
-     */
+	/**
+	 * Handle plugin installation. This method will only be called once, on first AM startup once the plugin
+	 * is included in the classpath. The {@link #onStartup()} method will be called after this one.
+	 *
+	 * No need to implement this unless your AuthNode has specific requirements on install.
+	 */
 	@Override
 	public void onInstall() throws PluginException {
 		super.onInstall();
 	}
 
-    /** 
-     * Handle plugin startup. This method will be called every time AM starts, after {@link #onInstall()},
-     * {@link #onAmUpgrade(String, String)} and {@link #upgrade(String)} have been called (if relevant).
-     * 
-     * No need to implement this unless your AuthNode has specific requirements on startup.
-     *
-     * @param startupType The type of startup that is taking place.
-     */
+	/**
+	 * Handle plugin startup. This method will be called every time AM starts, after {@link #onInstall()},
+	 * {@link #onAmUpgrade(String, String)} and {@link #upgrade(String)} have been called (if relevant).
+	 *
+	 * No need to implement this unless your AuthNode has specific requirements on startup.
+	 *
+	 * @param startupType The type of startup that is taking place.
+	 */
 	@Override
 	public void onStartup() throws PluginException {
 		super.onStartup();
 	}
 
-    /** 
-     * This method will be called when the version returned by {@link #getPluginVersion()} is higher than the
-     * version already installed. This method will be called before the {@link #onStartup()} method.
-     * 
-     * No need to implement this untils there are multiple versions of your auth node.
-     *
-     * @param fromVersion The old version of the plugin that has been installed.
-     */	
+	/**
+	 * This method will be called when the version returned by {@link #getPluginVersion()} is higher than the
+	 * version already installed. This method will be called before the {@link #onStartup()} method.
+	 *
+	 * No need to implement this untils there are multiple versions of your auth node.
+	 *
+	 * @param fromVersion The old version of the plugin that has been installed.
+	 */
 	@Override
 	public void upgrade(String fromVersion) throws PluginException {
+		pluginTools.upgradeAuthNode(SelectRoleNode.class);
 		super.upgrade(fromVersion);
 	}
 
-    /** 
-     * The plugin version. This must be in semver (semantic version) format.
-     *
-     * @return The version of the plugin.
-     * @see <a href="https://www.osgi.org/wp-content/uploads/SemanticVersioning.pdf">Semantic Versioning</a>
-     */
+	/**
+	 * The plugin version. This must be in semver (semantic version) format.
+	 *
+	 * @return The version of the plugin.
+	 * @see <a href="https://www.osgi.org/wp-content/uploads/SemanticVersioning.pdf">Semantic Versioning</a>
+	 */
 	@Override
 	public String getPluginVersion() {
 		return SelectRoleNodePlugin.currentVersion;
